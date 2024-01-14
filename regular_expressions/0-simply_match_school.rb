bf228dac869806380b6b9c4ee64c414d11bdddfa
tamandakaunda-15 <tamandakaunda900@gmail.com>
2024-01-14 14:49:59 +0000

    expressions

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-# Coomand line arguments
-input_string = ARGV[0] || "Neha loves School, that is why she is a School mentor :)"
-
-# Define the pattern of the regular expression
-pattern = /School/
-
-# Check if the input string matches the pattern
-match = input_string.match(pattern)
-
-## Print the result
-puts match ? match[0] : ''
+puts ARGV[0].scan(/School/).join
