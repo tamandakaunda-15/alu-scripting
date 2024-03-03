50d062846063fcf33df60bdc26039c7add7da4ab
myUsername <myemail@gmail.com>
2024-03-03 10:41:22 -0800

    api

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Ubuntu:20.04.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
