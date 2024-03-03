9bf71d66d0349a4b4809f7c9da91028d6d57139c
myUsername <myemail@gmail.com>
2024-03-03 10:01:20 -0800

    api advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""A  python function that queries the Reddit API"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-        results = response.json().get("data")
-        return results.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
